cf984abbbcd6bc6314992f1eb9cfe04d13162c85
Simon Yawin <simyawin@gmail.com>
2020-07-06 22:27:31 -0400

    test(Dollar): FInish Chapter 5
    Updated Dollar tests to be more concise.

--- kotlin-money/test/dollarTests.kt
+++ kotlin-money/test/dollarTests.kt
@@ -3,10 +3,8 @@ import kotlin.test.assertNotEquals
 
 fun testMultiplication() {
     val five = Dollar(5)
-    val ten = five.times(2)
-    val fifteen = five.times(3)
-    assertEquals(10, ten.amount)
-    assertEquals(15, fifteen.amount)
+    assertEquals(Dollar(10), five.times(2))
+    assertEquals(Dollar(15), five.times(3))
 }
 
 fun testEquality() {
